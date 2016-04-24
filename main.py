bc09016db98df42ed82f17fe38fbea9eb2480c5d
chabrowa <elessarin@gmail.com>
2016-04-24 22:57:27 +0100

    generating data file

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-import sys
+import urllib2
+import json
+import csv
+import numpy
 
-while True:
-    print "NANANANA BATMAN"
+fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package')
+results = json.loads(fp.read())
+fp.close()
+
+f = open("data.json","w")
+count = 0
+resources =[]
+
+for packageId in results:
+    if count < 5:
+        fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package/'+ packageId)
+        resource = json.loads(fp.read())
+        #f.write(json.dumps(resource))
+        resources.insert(-1, resource)
+        fp.close()
+        count = count + 1
+
+f.write(json.dumps(resources))
+f.close()

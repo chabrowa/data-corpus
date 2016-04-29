9503f8c6bd80b287f0cf54dd19d2aa80ddee1e37
chabrowa <elessarin@gmail.com>
2016-04-29 14:10:34 +0100

    data

--- index.py
+++ index.py
@@ -18,12 +18,8 @@ import json
 import csv
 import numpy
 
-#fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package')
-#results = json.loads(fp.read())
-
 with open('data.json') as data_file:
     results = json.load(data_file)
-#fp.close()
 
 numberOfPackages                = len(results)
 sumOfResources                  = 0
@@ -31,18 +27,13 @@ sumOfPackagesWthoutResources    = 0
 listOfResources                 = [];
 
 for resource in results:
-    #fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package/'+ packageId)
-    #resource = json.loads(fp.read())
     print resource["num_resources"]
     listOfResources.append(resource["num_resources"])
     sumOfResources = sumOfResources + resource["num_resources"]
     if resource["num_resources"] == 0:
         sumOfPackagesWthoutResources = sumOfPackagesWthoutResources + 1
-    #fp.close()
 
 
 print "Number of packages on data.gov.uk: " + str(numberOfPackages)
 print "Average number of resources per package: " + str(sumOfResources/numberOfPackages)
 print "Number of packages withour resources: " + str(sumOfPackagesWthoutResources)
-print sumOfResources
-print numberOfPackages
--- main.py
+++ main.py
@@ -7,18 +7,26 @@ fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package')
 results = json.loads(fp.read())
 fp.close()
 
-f = open("data.json","w")
 count = 0
+numberOfPackagesPerFile = 300
 resources =[]
 
 for packageId in results:
-    if count < 5:
-        fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package/'+ packageId)
-        resource = json.loads(fp.read())
-        #f.write(json.dumps(resource))
-        resources.insert(-1, resource)
-        fp.close()
-        count = count + 1
+    if count%numberOfPackagesPerFile == 0:
+        print "count: " + str(count/numberOfPackagesPerFile) + " i am making new file"
+        f = open("data/data" + str(count/numberOfPackagesPerFile) + ".json","w")
 
-f.write(json.dumps(resources))
-f.close()
+
+    fp = urllib2.urlopen('http://data.gov.uk/api/2/rest/package/'+ packageId)
+    resource = json.loads(fp.read())
+    #f.write(json.dumps(resource))
+    resources.insert(count%numberOfPackagesPerFile, resource)
+    fp.close()
+
+    if count%numberOfPackagesPerFile == numberOfPackagesPerFile-1:
+        print "count: " + str(count) + " i am closing the file"
+        f.write(json.dumps(resources))
+        resources =[]
+        f.close()
+
+    count = count + 1
